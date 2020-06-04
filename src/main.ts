f8b552b2577482f8f031baa3e5560e572543da33
Rafael Fagundes <rafaelcflima@gmail.com>
2020-06-04 18:41:03 -0300

    Documents added

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 require('dotenv').config();
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import swaggerOptions from './config/swagger.config';
 
 async function bootstrap() {
+  const logger = new Logger('@Main');
   const app = await NestFactory.create(AppModule);
 
   if (process.env.NODE_ENV && process.env.NODE_ENV === 'development') {
@@ -13,5 +15,7 @@ async function bootstrap() {
   }
 
   await app.listen(process.env.PORT);
+
+  logger.log(`Application listening on port: ${process.env.PORT}`);
 }
 bootstrap();
